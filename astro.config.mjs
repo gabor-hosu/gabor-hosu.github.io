a927e7c277964f85e3a4177c2c1af6a5229a8fcb
Gabor Hosu <gabor.hosu121@gmail.com>
2025-07-14 16:39:57 +0300

    Basic NavBar component added

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,17 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
+  redirects: {
+    "/": "/about",
+  },
+
+  integrations: [icon()],
 });
