603fb6c9cd6bab74dde2281b705148dd0d0c095a
Gabor Hosu <gabor.hosu121@gmail.com>
2025-07-26 19:07:10 +0300

    Preview functionality added with basic styling

--- src/content/config.ts
+++ src/content/config.ts
@@ -24,8 +24,25 @@ const technologiesCollection = defineCollection({
 
 const timelineCollection = defineCollection({});
 
+const projectCollection = defineCollection({
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      coverImage: z.object({
+        url: image(),
+      }),
+      date: z.object({
+        start: z.coerce.date(),
+        end: z.coerce.date(),
+      }),
+      tags: z.array(z.string()),
+    }),
+});
+
 export const collections = {
   profile: profileCollection,
   technologies: technologiesCollection,
   timeline: timelineCollection,
+  project: projectCollection,
 };
