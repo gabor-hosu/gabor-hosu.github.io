7dcad677e9c921d51a82315b1389062856a2a67c
Gábor Hosu <98151072+gabor-hosu@users.noreply.github.com>
2025-07-31 14:28:28 +0300

    Feature/projects page/5 (#6)
    * Preview functionality added with basic styling
    
    * Project pages added with basic styling
    
    * Custom lightbox added using custom rehype plugin
    
    * Major css refactoring
    
    * Add styling for video, images, and basic typographic elements

---
+++ src/plugins/rehype-add-lightbox-wrapper.js
@@ -0,0 +1,60 @@
+import { visit } from "unist-util-visit";
+
+let uniqueId = 0;
+
+export default function rehypeAddLightboxWrapper(allowedDirs) {
+  console.log("[info]: rehype-add-glightbox-wrapper plugin loaded");
+  return (tree, file) => {
+    visit(tree, { tagName: "figure" }, (node, index, parent) => {
+      const filepath = file.history?.[0] || "";
+
+      if (!allowedDirs.some((dir) => filepath.includes(dir))) {
+        return;
+      }
+
+      if (
+        !node.properties ||
+        !node.properties.className ||
+        !node.properties.className.includes("figure")
+      ) {
+        return;
+      }
+
+      const inputId = `zoom-toggle${uniqueId}`;
+      uniqueId++;
+
+      const wrapper = {
+        // div
+        type: "element",
+        tagName: "div",
+        properties: {
+          className: ["zoom-container"],
+        },
+        children: [
+          {
+            // checkbox
+            type: "element",
+            tagName: "input",
+            properties: {
+              type: "checkbox",
+              id: inputId,
+              className: ["zoom-toggle"],
+            },
+          },
+          {
+            // label
+            type: "element",
+            tagName: "label",
+            properties: {
+              for: inputId,
+              className: ["zoom-label"],
+            },
+            children: [node],
+          },
+        ],
+      };
+
+      parent.children[index] = wrapper;
+    });
+  };
+}
